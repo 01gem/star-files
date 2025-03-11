e48041e866fa32e7b185551bff158cef8feb8ace
01gem <gemdulduco13@gmail.com>
2025-03-11 15:07:50 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function menuClick(menuItem) {
+    alert(`You clicked on ${menuItem}`);
+}
+
+const banner = document.querySelector('.banner');
+const images = [
+    'URL("slogan1.gif")',
+    'URL("slogan2.gif")',
+    'URL("slogan3.gif")'
+];
+let index = 0;
+
+function changeBannerImage() {
+    banner.style.backgroundImage = images[index];
+    index = (index + 1) % images.length;
+}
+
+setInterval(changeBannerImage, 3000);
+changeBannerImage();
+
+document.addEventListener('DOMContentLoaded', function() {
+    const feedbackElement = document.querySelector('.feedback');
+    const email = feedbackElement.getAttribute('data-email');
+    const hoverContent = document.createElement('div');
+    hoverContent.className = 'hover-content';
+    hoverContent.innerHTML = `We love to hear about your feedback about us at <a href="mailto:${email}" style="color: #20C997;">AlphaCore Intelligence</a>`;
+    feedbackElement.appendChild(hoverContent);
+});
